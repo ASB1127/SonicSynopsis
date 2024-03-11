ec2a7245d8e9653e54119409fdcf93f1f67e35f4
abal@sfsu.edu” <amitsinghbal27@gmail.com>
2024-03-11 16:53:42 -0700

    Initial Commit

---
+++ SonicSynopsis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SonicSynopsis
+//
+//  Created by Amit Bal on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, there!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SonicSynopsis/SonicSynopsisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonicSynopsisApp.swift
+//  SonicSynopsis
+//
+//  Created by Amit Bal on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SonicSynopsisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
